b69a24f5713047f18df3625e6dae5f536eb23663
Richard Brown <richardtbrownjr@gmail.com>
2016-07-25 06:18:14 -0400

    update movie_lib with non working top movie

--- movies_lib.py
+++ movies_lib.py
@@ -2,47 +2,30 @@ import csv
 import os
 
 movie_rows = []
-# with open('u.item.csv') as f:
-#     fieldnames = ['movieid','movietitle','unknown', 'action']
-#     reader = csv.reader(f)
-#     headers = next(reader)
-#     row = next(reader)
-#     #print(row)
-    #
-#     for row in reader:
-#         movie_rows = row(row)
-#         movie_rows.append(movie_rows)
-#         print(row)
-# clear
-    # print(movies_rows[])
-    # return '{}: {}'.format(self.movieid, self.movietitle, self.unknown, self.action)
-
 
 with open('ml-100k/u.item', encoding='latin_1') as f:
 #with open('u.item', encoding='latin_1') as f:
     reader = csv.reader(f, delimiter='|')
     for row in reader:
-        #print(row)
         movie_rows.append(row)
-# print(movie_rows[:])
-# print(movie_rows[1:6])
-    #print(movie_rows[6][1])
+print(movie_rows[:])
+print(movie_rows[6][1])
+print(movie_rows[7][1])
+print(movie_rows[1681][1])
 
 user_id = []
 with open('ml-100k/u.user', encoding = 'latin_1') as f:
     reader = csv.reader(f, delimiter = '|')
     for row in reader:
-        #print(row)
         user_id.append(row)
 #print(user_id[:])
 
 ratings = []
 with open('ml-100k/u.data', encoding = 'latin_1') as f:
-    reader = csv.reader(f, delimiter = '|')
+    reader = csv.reader(f, delimiter = '\t')
     for row in reader:
-        #print(row)
         ratings.append(row)
-print(ratings[:])
+#print(ratings[:])
 
 
 class Movie:
@@ -51,26 +34,50 @@ class Movie:
     #def __init__(self, mid, title, ratings, av_ratings):
         self.mid = mid
         self.title = title
-        self.ratings = {}
-        self.av_ratings = ""
+
 
     def __str__(self):
-        return "{} Average Ratings = {} Ratings = {}". format(self.movietitle, self.avg_ratings, self.ratings)
+        return "Movie Title {} Average Rating {} Ratings {}". format(self.movietitle, self.avg_ratings, self.ratings)
 
 
     def create_movies_with_id_and_title(mid, movietitle, title, ratings, av_ratings):
         pass
 
+    def search_by_movie_id(mid):
+        #Search for movies by id in movie list
+        #return  movie_rows[mid].title
+        return movie_rows[mid][1]
+
 
 class User:
-    pass
-    # def __init__(self, row):
-    #     self.id = row[0]
-    #     self.age = row[1]
-    #     self.gender = row[3]
+    def __init__(self, uid, gender):
+        self.uid = uid
+        self.gender = gender
+
+    def __str__(self):
+        return "User Id {}". format(self.uid)
+
 
 class Ratings:
-    pass
+    def __init__(self, ratings):
+        self.ratings_rid = ratings
+        self.ratings = []
+        self.av_ratings = 0
+
+    def add_rating(self, ratings):
+        self.ratings.append(ratings)
+        self.av_ratings = sum(self.ratings)/len(self.ratings)
+
+    def __str__(self):
+        return "User_ID {} Movie ID {} Ratings {}". format(self.user_rid, self.movie_id, self.ratings_rid)
+    # def __str__(self):
+    #     return "Ratings {}". format(self.ratings_rid)
+
+    def top_rated_movies(number_of_movies, minum_number_of_ratings):
+        top_rated_movies = []
+        for movie in movie.title[movie]():
+            if len(movie.ratings) >= minum_number_of_ratings_num_ratings:
+                top_rated_movies.append(movie)
 
 # def euclidean_distance(v, w):
 #     """Given two lists, give the Euclidean distance between them on a scale
@@ -88,18 +95,34 @@ class Ratings:
 #
 #     return 1 / (1 + math.sqrt(sum_of_squares))
 
-print(movie_rows[0][1], movie_rows[0][0])
-print(movie_rows[1][1], movie_rows[1][0])
-print(movie_rows[2][1], movie_rows[2][0])
-print(movie_rows[3][1], movie_rows[3][0])
+# print(movie_rows[0][1], movie_rows[0][0])
+# print(movie_rows[1][1], movie_rows[1][0])
+# print(movie_rows[2][1], movie_rows[2][0])
+# print(movie_rows[3][1], movie_rows[3][0])
 
-# for i in range(len(ratings)):
-#     print(ratings[i])
-#     print('')
-# count = 0
-# for i in range(len(ratings)):
-#     if 't1' in ratings[i]:
-#         count = count + 1
-# print(count)
+print(' The one', end=' ')
+print(ratings[0][0])
 
-#if __name__ == '__main__':
+
+def display_rated_movies():
+    try:
+        number_of_movies = int(input("How many movies would you like to view? "))
+        minum_number_of_ratings = int(input("What's the minimum number of ratings? "))
+
+    except ValueError:
+        print("That's not a number!\n")
+
+    else:
+        print("RANK\tRATING\tTITLE")
+        for i, movie in enumerate(ratings_rid.top_rated_movies(numbmer_of_movies, minum_number_of_ratings), start=1):
+                print(i, "\t", movie.av_ratings, "\t", movie.title)
+
+def main():
+
+    display_rated_movies()
+
+#print('in main')
+#search_by_movie_id(1681)
+#
+if __name__ == '__main__':
+    main()
